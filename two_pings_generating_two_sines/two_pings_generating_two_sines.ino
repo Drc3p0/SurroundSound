bf4b76398e382b15741ead77306a0416f6fd8a1a
Stephen Wylie <stev-o@stev-o.us>
2016-10-31 21:16:42 -0500

    Smoothing and duplicating
    Adding smoothing code to low-pass filter the exact values from the sonar
    sensors.  Also duplicating the analog outputs 3 times, one for each device
    (thought I did this already?)

--- two_pings_generating_two_sines/two_pings_generating_two_sines.ino
+++ two_pings_generating_two_sines/two_pings_generating_two_sines.ino
@@ -11,6 +11,8 @@
 NewPing sonar1(PING_PIN_A, PING_PIN_A, MAX_DISTANCE);  //sonar[1] for amplitude
 NewPing sonar2(PING_PIN_B, PING_PIN_B, MAX_DISTANCE);  //sonar[2] for frequency
 
+#define JUMP_FACTOR 5
+
 //timing and sensor vals
 const unsigned int SECOND = 1000;
 const unsigned int PING_CHECK_INTERVAL = SECOND / 75;  //how often the ultrasonic sensor is checked
@@ -19,7 +21,11 @@ unsigned long nextTime = 0;
 unsigned int rawSine2 = 0;
 unsigned int rawSine1 = 0;
 byte rawPing1 = 0;
+byte limitedPing1 = 15;
+byte smoothedPing1 = 15;
 byte rawPing2 = 0;
+byte limitedPing2 = 15;
+byte smoothedPing2 = 15;
 byte whichPing = 1;
 
 //Teensy Audio System Design Tool
@@ -72,13 +78,10 @@ void loop() {
                 if (whichPing ==1) {
                         whichPing = 2;
                         rawPing1 = sonar1.ping_cm(); //raw ultrasonic value
-                        // drive it 3 times for the 3 devices we're sending data to
-                        analogWrite(3, rawPing1);
-                        analogWrite(5, rawPing1);
-                        analogWrite(9, rawPing1);
 
                       //sine generator interjected into 1st ping check
                         if (rawPing1 >= 1 && rawPing1 <= 30) { //if sensor is triggered within the detection range
+                                limitedPing1 = rawPing1;
                                 rawSine1 =  map(rawPing1, 0, 30, 20, 250);//map it to the frequency range desired...  could this be done on a logarithmic curve??
                                 
                                 //sine_fm1.frequency(rawSine1);
@@ -94,23 +97,28 @@ void loop() {
                         else {
                                 //Sine_ramper1 = 0;  //if sensor is outside of range, set sine to 0
                                 rawSine1 = 0;
-
+                                limitedPing1 = 15;
                         }
                         //sine_fm1.frequency(rawSine1);
                         sine_fm1.frequency(Sine_ramper1);
 
+                        if (limitedPing1 > smoothedPing1)
+                                smoothedPing1 += min(JUMP_FACTOR, limitedPing1 - smoothedPing1);
+                        else
+                                smoothedPing1 -= min(JUMP_FACTOR, smoothedPing1 - limitedPing1);
+                        // drive it 3 times for the 3 devices we're sending data to
+                        analogWrite(3, smoothedPing1);
+                        analogWrite(5, smoothedPing1);
+                        analogWrite(9, smoothedPing1);
                 }
                 //get values from ping 2
                 else {
                         rawPing2 = sonar2.ping_cm(); //raw ultrasonic value
                         whichPing = 1;
-                        // drive it 3 times for the 3 devices we're sending data to
-                        analogWrite(4, rawPing2);
-                        analogWrite(6, rawPing2);
-                        analogWrite(10, rawPing2);
 
                         //sine generator interjected into 2nd ping check
                         if (rawPing2 >= 1 && rawPing2 <= 30) { //if sensor is triggered within the detection range
+                                limitedPing2 = rawPing2;
                                 rawSine2 =  map(rawPing2, 0, 30, 20, 250);//map it to the frequency range desired...  could this be done on a logarithmic curve??
                                 
                                 //sine_fm1.frequency(rawSine2);
@@ -127,11 +135,19 @@ void loop() {
                         else {
                                 //Sine_ramper2 = 0;  //if sensor is outside of range, set sine to 0
                                 rawSine2 = 0;
-
+                                limitedPing2 = 15;
                         }
                         //sine_fm1.frequency(rawSine2);
                         sine_fm1.frequency(Sine_ramper2);
-                     
+
+                        if (limitedPing2 > smoothedPing2)
+                                smoothedPing2 += min(JUMP_FACTOR, limitedPing2 - smoothedPing2);
+                        else
+                                smoothedPing2 -= min(JUMP_FACTOR, smoothedPing2 - limitedPing2);
+                        // drive it 3 times for the 3 devices we're sending data to
+                        analogWrite(4, smoothedPing2);
+                        analogWrite(6, smoothedPing2);
+                        analogWrite(10, smoothedPing2);
                 }
         }
 
